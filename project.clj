3342488775b50337f4574b50acac741d79c84a62
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-08 09:40:55 +0200

    add additional parameters to project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,12 @@
   :description "Config module for dataMos."
   :url "http://theinfotect.org/datamos"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
-            :url "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
+            :url  "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.volenta/datamos "0.1.3"]
-                 [mount "0.1.11"]])
+                 [mount "0.1.11"]]
+  :main datamos.config.core
+  :aot [datamos.config.core]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev     {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]}})
--- src/config_component/core.clj
+++
@@ -1,6 +0,0 @@
-(ns config-component.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
