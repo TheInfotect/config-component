9cf2e00a319e96b7f3fc142f93aec712463447fd
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-08 16:08:34 +0200

    bumped version to 0.1.4, equal to the version of dataMos

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.volenta/datamos.config "0.1.0-SNAPSHOT"
+(defproject org.volenta/datamos.config "0.1.4"
   :description "Config module for dataMos."
   :url "http://theinfotect.org/datamos"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
