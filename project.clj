424a384b980a1b9da0b19e76b427580b444343e3
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-08 08:23:30 +0200

    add mount and datamos to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject config-component "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+(defproject org.volenta/datamos.config "0.1.0-SNAPSHOT"
+  :description "Config module for dataMos."
+  :url "http://theinfotect.org/datamos"
+  :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojars.the-infotect/datamos "0.1.2"]
+                 [mount "0.1.11"]])
