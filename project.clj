2e66d4fb9edb7b550474ff6abfdba37b4bceeed8
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-11 14:46:15 +0200

    datamos v0.1.5.7, config v0.1.5.3

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject org.volenta/datamos.config "0.1.5.2"
+(defproject org.volenta/datamos.config "0.1.5.3"
   :description "Config module for dataMos."
   :url "http://theinfotect.org/datamos"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
             :url  "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.volenta/datamos "0.1.5.6"]
+                 [org.volenta/datamos "0.1.5.7"]
                  [mount "0.1.11"]
                  [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot datamos.config.core
   :aot [datamos.config.core]
+  :test-paths ["test"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]}}
