f5bce86ad1ee838a4e7bee63b62c730b18d20b9c
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-15 11:13:50 +0200

    bump dataMos to v0.1.5.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.volenta/datamos.config "0.1.5.4"
+(defproject org.volenta/datamos.config "0.1.5.4-SNAPSHOT"
   :description "Config module for dataMos."
   :url "http://theinfotect.org/datamos"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
             :url  "https://www.gnu.org/licenses/agpl-3.0.nl.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.volenta/datamos "0.1.5.7"]
+                 [org.volenta/datamos "0.1.5.8-SNAPSHOT"]
                  [mount "0.1.11"]
                  [com.taoensso/timbre "4.10.0"]
                  [org.clojure/tools.namespace "0.2.11"]]
@@ -14,4 +14,6 @@
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]}}
   :repositories [["releases" {:url "https://clojars.org/repo"
-                              :creds :gpg}]])
+                              :creds :gpg}]]
+  :repl-options {:host "127.0.0.1"
+                 :port 12311})
